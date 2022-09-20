d246151ea8d5a08fc2512893796ee5dd15f24125
John Biolo <johnbiolo@148-100-154-180.foxnet.marist.edu>
2022-09-20 16:04:55 -0400

    adding all of my completed labs

---
+++ assignments/basics/primitives.py
@@ -0,0 +1,23 @@
+def get_int():
+    '''
+    Returns a variable of type 'int'.
+    '''
+    return 2
+
+def get_float():
+    '''
+    Returns a variable of type 'float'.
+    '''
+    return 2.7
+
+def get_str():
+    '''
+    Returns a variable of type 'str'.
+    '''
+    return "My name is John"
+
+def get_bool():
+    '''
+    Returns a variable of type 'bool'.
+    '''
+    return True
